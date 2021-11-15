49f2d1bd7eee45e8e6ccaa9787e2ffdbcb25e3f0
JSmithStaff <90048177+JSmithStaff@users.noreply.github.com>
2021-11-15 10:25:14 +0000

    Update Test_Ultrasonic.java

--- Test_Ultrasonic.java
+++ Test_Ultrasonic.java
@@ -4,8 +4,8 @@
 public class Test_Ultrasonic{
 	public static void main(String[] args) throws Exception{
 		PiJavaUltrasonic sonic=new PiJavaUltrasonic(
-			0,//ECO 11
-			1,//TRIG 22
+			6,//ECO 11
+			23,//TRIG 22
 			1000,//REJECTION_START ; long
 			23529411 //REJECTION_TIME ; long
 		);
